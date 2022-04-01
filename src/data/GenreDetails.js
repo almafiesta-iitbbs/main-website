dd9f501e8a5e999ac046c593f36c9cde39ef2841
Shrirang Deshmukh <deshmukhshrirang01@gmail.com>
2022-04-01 20:59:20 +0530

    Added Event Details

--- src/data/GenreDetails.js
+++ src/data/GenreDetails.js
@@ -4,6 +4,8 @@ import FineArtsPhoto from "../images/final-main-page/events/fine_arts.jpg";
 import DramaticsPhoto from "../images/final-main-page/events/drams-4.jpg";
 import MusicPhoto from "../images/final-main-page/events/music-61.jpg";
 import LiteraturePhoto from "../images/final-main-page/events/mun-2.jpg";
+import ESportsPhoto from "../images/final-main-page/events/egames.jpg";
+import PhotographyPhoto from "../images/final-main-page/events/photography.jpg";
 
 const dance = (
   <div>
@@ -19,106 +21,126 @@ const dance = (
     short video of your dance and send us your entries to win exciting prizes.
   </div>
 );
-
-const music = (
-  <div>
-    Any festival is incomplete without music. And when it comes to Alma fiesta,
-    music is its soul. <br />
+const finearts = (
+  <>
+    "I found I could say things with color and shapes that I couldn't say any
+    other way - things I had no words for."
     <br />
+    <b> - Georgia O'Keeffe</b>
+    <br />
+    Art is an expression of life itself, many experiences that cannot be
+    described in words are portrayed by art. There is no perfection, art is what
+    the eye perceives it to be.
+    <br />
+    Keeping this in mind, Alma Fiesta'22 with the theme of a utopia presents
+    fine art events from different mediums, calling every artist to express
+    themselves with their creativity and imagination.
+    <br />
+  </>
+);
+const photography = (
+  <>
+    Your utopia is your own envisioning of the perfect world and a camera aids
+    tell the story of your imagination out of reality. So take out your cameras
+    and steal some moments of your version of that perfection, since alma fiesta
+    is here with a variety of interesting
+    <br />
+    <b>photography and cinematography events like:</b>
+    <br />
+    <ul>
+      <ol>Photostory (online photojournalism)</ol>
+      <ol>Trailor making</ol>
+      <ol>Showreel (online short film making)</ol>
+      <ol>Photography contest</ol>
+      <ol>AD- making competition </ol>
+    </ul>
+  </>
+);
+const gaming = (
+  <>
+    Assemble gamers! because Team Alma Fiesta brings to you exceptional events
+    where you can let out your precision and skills and leave the people be
+    astonished. Get ready to exhibit your gaming skills.
+    <b>Events we have gathered:</b>
+    <ul>
+      <ol>CSGO</ol>
+      <ol>CODM</ol>
+      <ol>Valorant</ol>
+      <ol>Rocket Leauge</ol>
+    </ul>
+    <br></br>
+    Let's see if you can shoot precisely to the top place! So what are you
+    waiting for!! Come n play! It's a call to the gamer in you!
+  </>
+);
+const fashion = (
+  <>
+    "What you wear is how you present yourself to the world, especially today
+    when human contacts are so quick. Fashion is instant language."
+    <br />
+    <b>-Miuccia Prada.</b>
+    <br />
+    Alma Fiesta brings you the opportunity to express yourself in this instant
+    language. Be it dress designing or ramp walking, “The Vogue Spectra” is a
+    competition covering it all. Participate in this competition and get a
+    chance to exhibit your fashion aesthetics to the world. For more information
+    and registration, visit our website-
+  </>
+);
+const literature = (
+  <>
+    Gear up your craze for fun-filled literature-based events. Let your inner
+    creativity out because Team Alma Fiesta brings to you exceptional events
+    where you can let out your talent and skills and leave the people be
+    astonished.
+    <br />
+    <b>Events we have gathered:</b>
+    <ul>
+      <ol>Gone in 60 seconds</ol>
+      <ol>Shipwrecked</ol>
+      <ol>Online MUN</ol>
+      <ol>Alt-End</ol>
+      <ol>Fantasy Quiz</ol>
+      <ol>Kavya Path</ol>
+    </ul>
+  </>
+);
+const music = (
+  <>
+    Any festival is incomplete without music. And when it comes to Alma fiesta,
+    music is its soul. So, here we bring the most exhilarating and electrifying
+    range of music competitions.
+    <br />
+    <b>The platter includes:</b>
+    <ul>
+      <ol>Dil Se</ol>
+      <ol>Twist the Lyrics</ol>
+      <ol>Rock On!</ol>
+    </ul>
     Don't be shy, show your talents and get the opportunity to be a rockstar.
     Because it's more than music to us!
-  </div>
+  </>
 );
-
 const dramatics = (
-  <div>
-    "The world's a stage and each one of us an actor. Alma Fiesta's the theatre
-    that everyone is after" <br />
+  <>
+    <b>
+      "The world's a stage and each one of us an actor. Alma Fiesta's the
+      theatre that everyone is after"
+    </b>
     <br />
     What's life without emotions and drama? Nothing but a blank canvas void of
     all the colours. Truly depicting the dream of a utopian world, Alma Fiesta
     presents to you a fun set of events celebrating each hue to the human
     emotion that fills our life with the vibrance that it needs.
-  </div>
-);
-
-const literature = (
-  <div>
-    Gear up your craze for fun-filled literature-based events.
     <br />
-    <br />
-    Let your inner creativity out because Team Alma Fiesta brings to you
-    exceptional events where you can let out your talent and skills and leave
-    the people be astonished.
-  </div>
-);
-
-const photography = (
-  <div>
-    Your Utopia is your own envisioning of the perfect world and a camera aids
-    tell the story of your imagination out of reality.
-    <br />
-    <br />
-    So take out your cameras and steal some moments of your version of that
-    perfection, since alma fiesta is here with a variety of interesting
-    photography and cinematography events
-  </div>
-);
-
-const finearts = (
-  <div>
-    "I found I could say things with color and shapes that I couldn't say any
-    other way - things I had no words for." <br />-<b>Georgia O'Keeffe</b>
-    <br />
-    <br />
-    Art is an expression of life itself, many experiences that cannot be
-    described in words are portrayed by art. There is no perfection, art is what
-    the eye perceives it to be. Keeping this in mind, Alma Fiesta'22 with the
-    theme of a utopia presents fine art events from different mediums, calling
-    every artist to express themselves with their creativity and imagination.
-  </div>
-);
-
-const gaming = (
-  <div>
-    Assemble gamers! because Team Alma Fiesta brings to you exceptional events
-    where you can let out your precision and skills and leave the people be
-    astonished. Get ready to exhibit your gaming skills.
-    <br />
-    <br />
-    Let's see if you can shoot precisely to the top place! So what are you
-    waiting for!! Come n play! It's a call to the gamer in you.
-  </div>
-);
-
-const fashion = (
-  <div>
-    "What you wear is how you present yourself to the world, especially today
-    when human contacts are so quick. Fashion is instant language." <br />-
-    <b>Miuccia Prada.</b>
-    <br />
-    <br />
-    Alma Fiesta brings you the opportunity to express yourself in this instant
-    language. Be it dress designing or ramp walking, “The Vogue Spectra” is a
-    competition covering it all. Participate in this competition and get a
-    chance to exhibit your fashion aesthetics to the world.
-  </div>
-);
-
-const food = (
-  <div>
-    DESCRIPTION
-    <br />
-    <br />
-  </div>
-);
-
-const filler = (
-  <div>
-    {" "}
-    DESCRIPTION <br />
-    <br />.
-  </div>
+    <b>The event roster includes:</b>
+    <ul>
+      <ol>Pont des émotions</ol>
+      <ol>WordSmith</ol>
+      <ol>Spoof-O-Mania</ol>
+      <ol>Audio Play</ol>
+    </ul>
+  </>
 );
 
 const genreDetails = [
@@ -143,7 +165,7 @@ const genreDetails = [
     description: literature,
   },
   {
-    // imageSrc: `${imageSrcc}`,
+    imageSrc: PhotographyPhoto,
     title: "Photography",
     description: photography,
   },
@@ -153,7 +175,7 @@ const genreDetails = [
     description: finearts,
   },
   {
-    // imageSrc: `${imageSrcc}`,
+    imageSrc: ESportsPhoto,
     title: "ESports",
     description: gaming,
   },
@@ -162,16 +184,16 @@ const genreDetails = [
     title: "Fashion",
     description: fashion,
   },
-  {
-    // imageSrc: `${imageSrcc}`,
-    title: "Food",
-    description: food,
-  },
-  {
-    // imageSrc: `${imageSrcc}`,
-    title: "Filler",
-    description: filler,
-  },
+  // {
+  //   // imageSrc: `${imageSrcc}`,
+  //   title: "Food",
+  //   description: food,
+  // },
+  // {
+  //   // imageSrc: `${imageSrcc}`,
+  //   title: "Filler",
+  //   description: filler,
+  // },
 ];
 
 export default genreDetails;
